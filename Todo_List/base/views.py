8463eac90cf4541b8ceb06fc041347e83b034277
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-03 18:13:21 +0530

    user content seperation from other users

--- Todo_List/base/views.py
+++ Todo_List/base/views.py
@@ -7,6 +7,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # imported from this app
 from .models import Task
@@ -20,29 +21,41 @@ class Base_Login(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):   # Listview return a template with query set of data
+class TaskList(LoginRequiredMixin, ListView):   # LoginRequiredMixin will check login but redirect user to default not login page so we have to set it and Listview return a template with query set of data
     model = Task
     context_object_name = 'tasks'   #change the object list to tasks
+
+    # ensure user can only get their data nor another user data
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # context set to orignal value | get_context_data making sure we are inheriting from the orignal item
+        context['tasks'] = context['tasks'].filter(user=self.request.user) # Filtering tasks according to user who created them
+        context['count'] = context['tasks'].filter(complete=False).count()   # count of incomplete items
+        return context
+
     template_name = 'base/tasks.html'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'  # include all the fields of the model
+    fields = ['title', 'description', 'complete', 'quote']
     success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
     template_name = 'base/create.html'
 
-class TaskUpdate(UpdateView):
+    def form_valid(self, form): # overriding the value so user set default not give option of choosing user to add task
+        form.instance.user = self.request.user  # ensure that it's logined user
+        return super(TaskCreate, self).form_valid(form) 
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'  # include all the fields of the model
+    fields = ['title', 'description', 'complete', 'quote']
     success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
     template_name = 'base/create.html'
 
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
