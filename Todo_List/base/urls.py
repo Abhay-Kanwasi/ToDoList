15ecfd515f89c4e6842ad4a2cab9abb5fa03f859
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-03 17:21:14 +0530

    CRUD functionality

--- Todo_List/base/urls.py
+++ Todo_List/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from . import views
-
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, Base_Login
 urlpatterns = [
-    path('',views.taskList, name='list'),
+    path('login/', Base_Login, name="login"),
+    path('',TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/',TaskDetail.as_view(), name='task'),
+    path('create-task/',TaskCreate.as_view(), name='create-task'),
+    path('update-task/<int:pk>/',TaskUpdate.as_view(), name='update-task'),
+    path('delete-task/<int:pk>/',TaskDelete.as_view(), name='delete-task'),
 ]
--- Todo_List/base/views.py
+++ Todo_List/base/views.py
@@ -1,5 +1,50 @@
+# Default 
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def taskList(request):
-    return HttpResponse('TO DO LIST')
+# imported from built-in django modules
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+# imported from this app
+from .models import Task
+
+
+class Base_Login(LoginView):
+    template_name = 'base/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class TaskList(ListView):   # Listview return a template with query set of data
+    model = Task
+    context_object_name = 'tasks'   #change the object list to tasks
+    template_name = 'base/tasks.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'  # include all the fields of the model
+    success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
+    template_name = 'base/create.html'
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'  # include all the fields of the model
+    success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
+    template_name = 'base/create.html'
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')  # it redirect's our user to a certain page
+    template_name = 'base/delete.html'
+
