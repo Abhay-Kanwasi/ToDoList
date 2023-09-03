6197e50122294b97bb473e8ff8136803b70c1f52
Abhay-Kanwasi <abhaykanwasi123@gmail.com>
2023-09-03 18:13:02 +0530

    login functionality

--- Todo_List/base/urls.py
+++ Todo_List/base/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, Base_Login
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
-    path('login/', Base_Login, name="login"),
+    path('login/', Base_Login.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name="logout"),
+
     path('',TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/',TaskDetail.as_view(), name='task'),
     path('create-task/',TaskCreate.as_view(), name='create-task'),
